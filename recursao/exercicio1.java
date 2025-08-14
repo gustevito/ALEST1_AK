9decc3224d45787daca9a5566c7ea5a7fdb4438d
gustevito <dev.gustavo.flores@gmail.com>
2025-08-14 12:52:41 -0300

    eu sou burro

---
+++ recursao/exercicio1.java
@@ -0,0 +1,20 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+        System.out.println(funcaoIt());
+    }
+
+    public static int funcaoIt(){
+        int n = 5;
+        int[] v = new int[n];
+        int count = 0;
+
+        for (int i=0; i<n; i++) {
+            count =+ v[n];
+            System.out.println(count);
+        } return count;
+    }
+
+    /*public int funcaoRec(){
+        
+    }*/
+}
---
+++ tarefa contagem/contagem.java
@@ -0,0 +1,11 @@
+public class contagem {
+
+    public static main(String[] args) {
+        int valor = 15;
+        int[] soma = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < soma.length; i++) {
+            int[] vetor = 
+        }
+    }
+}
