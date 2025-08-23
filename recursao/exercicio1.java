8b85262fc0536bb03faf87892330457466bddf6f
gustevito <dev.gustavo.flores@gmail.com>
2025-08-23 02:20:31 -0300

    sla

--- recursao/exercicio1.java
+++ recursao/exercicio1.java
@@ -1,20 +1,37 @@
 public class exercicio1 {
     public static void main(String[] args) {
-        System.out.println(funcaoIt());
+        int[] entradas = {10, 20, 30, 40, 50, 100};
+
+        for (int n : entradas) {
+            // iterativa
+            long inicioIt = System.nanoTime();
+            int resultadoIt = funcaoIt(n);
+            long fimIt = System.nanoTime();
+            long tempoIt = fimIt - inicioIt;
+
+            // recursiva
+            long inicioRec = System.nanoTime();
+            int resultadoRec = funcaoRec(n);
+            long fimRec = System.nanoTime();
+            long tempoRec = fimRec - inicioRec;
+
+            System.out.println("N = " + n);
+            System.out.println("ITERATIVA: \nResultado = " + resultadoIt + " \nTempo = " + tempoIt + " ns");
+            System.out.println("\nRECURSIVA: \nResultado = " + resultadoRec + " \nTempo = " + tempoRec + " ns");
+            System.out.println("---------------------------");
+        }
     }
 
-    public static int funcaoIt(){
-        int n = 5;
-        int[] v = new int[n];
-        int count = 0;
-
-        for (int i=0; i<n; i++) {
-            count =+ v[n];
-            System.out.println(count);
-        } return count;
+    public static int funcaoIt(int n) {
+        int soma = 0;
+        for (int i = 1; i <= n; i++) {
+            soma += i;
+        }
+        return soma;
     }
 
-    /*public int funcaoRec(){
-        
-    }*/
+    public static int funcaoRec(int n) {
+        if (n == 1) return 1;
+        return n + funcaoRec(n - 1);
+    }
 }
---
+++ tarefa_contagem/contagem.java
@@ -0,0 +1,12 @@
+public class contagem
+{
+
+    public static main(String[] args)
+    {
+        int vetor[10];
+        vetor = {1,2,3,4,5};
+        
+        
+    }
+
+}
