b791829a8350304ef7a8bf2936be42281c00e10f
gustevito <dev.gustavo.flores@gmail.com>
2025-08-25 16:43:16 -0300

    moodle

---
+++ moodle/m1/cod.java
@@ -0,0 +1,40 @@
+public class cod {
+    public static int f1(int n) {
+        int r = 0;
+        int op = 0;
+        for (int i = 1; i < n; i++) {
+            r = r + 1;
+            op = op + 1;
+        }
+        return op;
+    }
+
+    public static int f2(int n) {
+        int r = 0;
+        int op = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                op = op + 1;
+                r = r + 2;
+            }
+        }
+        return op;
+    }
+
+    //public static int f3(int n)
+    //public static int f4(int n)
+    //public static int f5(int n)
+
+
+    public static void main(String[] args) {
+        int op = 0;
+        System.out.println("N    |#OP");
+
+        //para cada funcao descomente a linha para chamar a funcao desejada
+        for (int n = 0; n <= 100; n+=10) {
+            op = f1(n);
+            //op = f2(n);
+            System.out.println(n + "    |" + op);
+        }
+    }
+}
---
+++ moodle/m2/somatorio.java
@@ -0,0 +1,44 @@
+public class somatorio {
+    //Exemplo: para n=5 -> 1+2+3+4+5=15
+    // Variáveis para contagem de operações
+    static int iterativaOperacoes = 0;
+    static int recursivaOperacoes = 0;
+
+    // Função iterativa
+    public static long funcaoIt(int n) {
+        long soma = 0;
+        iterativaOperacoes++; // Var contagem
+        for (int i = 1; i <= n; i++) {
+            soma += i;
+            /* Cada operação de soma dentro do laço for e a própria iteração são contadas como operações individuais.*/
+            iterativaOperacoes += 2;
+        }
+        return soma;
+    }
+
+    // Função recursiva
+    public static long funcaoRe(int n) {
+        recursivaOperacoes++; // Var contagem
+        if (n == 1) {
+            /* Cada chamada recursiva e cada operação de soma são contadas. A verificação da condição (n == 1) também é contada como uma operação.*/
+            recursivaOperacoes++;
+            return 1;
+        }
+        return n + funcaoRe(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] entradas = {10, 20, 30, 40, 50, 100};
+
+        System.out.println("Tamanho\tOperações Iterativa\tOperações Recursiva");
+
+        for (int n : entradas) {
+            iterativaOperacoes = 0;
+            recursivaOperacoes = 0;
+            funcaoIt(n);
+            funcaoRe(n);
+            System.out.println(iterativaOperacoes);
+            System.out.println(recursivaOperacoes);
+        }
+    }
+}
