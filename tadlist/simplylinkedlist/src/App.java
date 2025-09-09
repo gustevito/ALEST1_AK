a5f0ea58d4c2d57f97a0ecb95800c54fdf23d04c
gustevito <dev.gustavo.flores@gmail.com>
2025-09-09 12:07:32 -0300

    lista simplesmente encadeada

---
+++ tadlist/simplylinkedlist/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) {
+        LinkedListOfInteger lista = new LinkedListOfInteger();
+
+      //  System.out.println("Tamanho da lista: " + lista.size());
+   //     System.out.println("Lista Vaiza? " + lista.isEmpty());
+
+        System.out.println("Adicionando elementos no final da lista...");
+
+        lista.add(2);
+        lista.add(4);
+        lista.add(6);
+        lista.add(8);
+        lista.add(10);
+        lista.add(12);
+        System.out.println(lista);
+        System.out.println("--> Elementos adicionados com sucesso!");
+
+    //    System.out.println("Tamanho da lista: " + lista.size());
+    //    System.out.println("Lista Vaiza? " + lista.isEmpty());
+
+
+        //System.out.println("Lista Vaiza? " + lista.isEmpty());
+
+    //    System.out.println("Elemento armazenado na "
+      //          + "segunda posicao da lista: " +lista.get(1));
+
+//        System.out.println("Elemento armazenado na "
+//                + "ultima posicao da lista: " +lista.get(lista.size()-1));
+
+    }
+}
---
+++ tadlist/simplylinkedlist/src/LinkedListOfInteger.java
@@ -0,0 +1,114 @@
+public class LinkedListOfInteger {
+
+    // Classe interna Node
+    private class Node {
+        public Integer element;
+        public Node next;
+
+        public Node(Integer element) {
+            this.element = element;
+            next = null;
+        }
+
+        public Node(Integer element, Node next) {
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    // Referência para o primeiro elemento da lista encadeada.
+    private Node head;
+    // Referência para o último elemento da lista encadeada.
+    private Node tail;
+    // Contador para a quantidade de elementos que a lista contem.
+    private int count;
+
+    // Construtor da lista
+    public LinkedListOfInteger() {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    // Esvazia a lista
+
+    public void clear() {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    // Adiciona um elemento ao final da lista.
+
+    public void add(Integer element) {
+        Node n = new Node(element);
+        if (head == null) {
+            head = n;
+        } else {
+            tail.next = n;
+        }
+        tail = n;
+        count++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        Node aux = head;
+
+        while (aux != null) {
+            s.append(aux.element.toString());
+            s.append("\n");
+            aux = aux.next;
+        }
+
+        return s.toString();
+    }
+
+    ///////////////////////////////////////////////////
+    //// EXERCICIOS - VEJA SLIDES E ENUNACIADO
+    ///////////////////////////////////////////////////
+
+    public boolean isEmpty() {
+        if (head == null) {
+            return ture;
+        }
+        return false;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    // 3 - implemente o método get
+    /*
+     * Retorna o elemento de uma determinada posicao da lista.
+     * 
+     * @param index a posição da lista
+     * 
+     * @return o elemento da posicao especificada
+     * 
+     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
+     */
+
+    public int get(int index) {
+        if (index < 0 || index >= size()) {
+            throw IndexOutOfBoundsException();
+        }
+
+    }
+
+    /*
+     * Exemplo - veja o main
+     * Lista:
+     * 2
+     * 4
+     * 8
+     * lista.get(1)
+     * Elemento na segunda posicao da lista: 4
+     */
+
+    // assinatura do metodo
+    // public Integer get(int index)
+}
