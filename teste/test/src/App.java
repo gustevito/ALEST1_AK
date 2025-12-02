e337155cdd7e6555c686e544ae5bfc2ab557445b
gustevito <dev.gustavo.flores@gmail.com>
2025-12-02 16:25:45 -0300

    freq test

--- teste/test/src/App.java
+++ teste/test/src/App.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] A = { 1, 2, 3, 4, 6 };
+        int[] A = { 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 4, 6 };
         System.out.println(solution(A));
+        System.out.println(frequencia(A));
     }
 
     public static int solution(int[] A) {
@@ -21,5 +23,13 @@ public class App {
         }
 
         return menor;
+    }
+
+    public static String frequencia(int[] A) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int num : A) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        return freq.toString();
     }
 }
